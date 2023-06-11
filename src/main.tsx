826bf08fadac5edfcab879b7c49847d666606710
Goran <goran.rihter@gmail.com>
2023-06-11 15:21:46 +0200

    checkpoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './_app.tsx';
+import App from './app.tsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
