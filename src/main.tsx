5172e3d0a92fc02165716a9c70368e70f990ff69
Goran <goran.rihter@gmail.com>
2023-06-11 13:45:03 +0200

    Implement Auth0 login and logout in App component, prettier, stylelint configs, format on save

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Auth0Provider
+            domain="dev-qhgi5fbx6fuxt8pk.us.auth0.com"
+            clientId="I4U9JyKl1dy1LVKsnFHa0M6Vt1jFS83H"
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+            }}
+        >
+            <App />
+        </Auth0Provider>
+    </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-todo/',
-  plugins: [react()],
+    base: '/react-todo/',
+    plugins: [react()],
 })
